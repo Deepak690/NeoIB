47a87e62c8a28ca4af30b3489c3611977611b567
Deepak690 <dipak.6990@gmail.com>
2016-07-30 01:16:08 +0530

    Add files via upload

---
+++ www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('cartCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('dashboardCtrl', function($scope) {
+
+})
+   
+.controller('page7Ctrl', function($scope) {
+
+})
+   
+.controller('myProfileCtrl', function($scope) {
+
+})
+   
+.controller('accountStatementCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,93 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('cart', {
+    url: '/page2',
+    templateUrl: 'templates/cart.html',
+    controller: 'cartCtrl'
+  })
+
+  .state('cloud', {
+    url: '/page3',
+    templateUrl: 'templates/cloud.html',
+    controller: 'cloudCtrl'
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/page5',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('menu.dashboard', {
+    url: '/page6',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/dashboard.html',
+        controller: 'dashboardCtrl'
+      }
+    }
+  })
+
+  .state('page7', {
+    url: '/page7',
+    templateUrl: 'templates/page7.html',
+    controller: 'page7Ctrl'
+  })
+
+  .state('menu.myProfile', {
+    url: '/page8',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/myProfile.html',
+        controller: 'myProfileCtrl'
+      }
+    }
+  })
+
+  .state('menu.accountStatement', {
+    url: '/page10',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/accountStatement.html',
+        controller: 'accountStatementCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/')
+
+  
+
+});
